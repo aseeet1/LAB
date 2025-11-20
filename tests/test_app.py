1a2785806340eae7b998553ffbf94964070f38f7
aseeet1 <toleukhanovaset@gmail.com>
2025-11-20 15:31:55 +0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,14 @@
-from app import add
-from app import kobeitu
+from app import add, kobeitu, alu, bolu
+
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_kobeitu():
     assert kobeitu(4, 5) == 20
+
+def test_alu():
+    assert alu(7, 6) == 1
+
+def test_bolu():
+    assert bolu(15, 3) == 5
