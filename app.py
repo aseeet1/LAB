520fa906908dad18acd3e1d9853811132f364160
aseeet1 <toleukhanovaset@gmail.com>
2025-11-20 15:29:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 def add(a, b):
     return a + b
 
-if __name__ == "__main__":
-    print(add(2, 3))
-
-
 def kobeitu(z, x):
     return z * x
+
+def alu(n, m):
+    return n - m
+
+def bolu(j, l):
+    return j // l
+
 if __name__ == "__main__":
+    print(add(2, 3))
     print(kobeitu(4, 5))
+    print(alu(7, 6))
+    print(bolu(15, 3))
